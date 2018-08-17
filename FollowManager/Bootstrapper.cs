545067212b5c0b818817eb5f596eb906b8786066
poketorena <poketorena@users.noreply.github.com>
2018-08-17 22:16:03 +0900

    プロジェクト ファイルを追加します。

---
+++ FollowManager/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using FollowManager.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace FollowManager
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
---
+++ FollowManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FollowManager.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
