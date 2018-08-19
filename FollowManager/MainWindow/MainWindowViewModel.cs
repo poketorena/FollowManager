c77629d564ab342dc3d03870b5d169326da8300a
poketorena <poketorena@users.noreply.github.com>
2018-08-19 20:25:33 +0900

    Change Title

--- FollowManager/MainWindow/MainWindowViewModel.cs
+++ FollowManager/MainWindow/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace FollowManager.MainWindow
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Follow Manager";
         public string Title
         {
             get { return _title; }
