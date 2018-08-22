c8ad2858dbc461ccce61d934a268deff0565c039
poketorena <poketorena@users.noreply.github.com>
2018-08-22 21:37:11 +0900

    Add FollowTypeConverters

---
+++ FollowManager/Converters/FollowType2ColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using FollowManager.Account;
+
+namespace FollowManager.Converters
+{
+    public class FollowType2ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(FollowType))
+            {
+                throw new ArgumentException();
+            }
+
+            switch ((FollowType)value)
+            {
+                case FollowType.Follow:
+                    // 水色
+                    return new SolidColorBrush((Color)new ColorConverter().ConvertFrom("#03A9F4"));
+                case FollowType.Follower:
+                    // ピンク
+                    return new SolidColorBrush((Color)new ColorConverter().ConvertFrom("#FF4081"));
+                case FollowType.Mutual:
+                    // オレンジ
+                    return new SolidColorBrush((Color)new ColorConverter().ConvertFrom("#FF5722"));
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FollowManager/Converters/FollowType2StringConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using FollowManager.Account;
+
+namespace FollowManager.Converters
+{
+    public class FollowType2StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(FollowType))
+            {
+                throw new ArgumentException();
+            }
+
+            switch ((FollowType)value)
+            {
+                case FollowType.Follow:
+                    return "片思い";
+                case FollowType.Follower:
+                    return "ファン";
+                case FollowType.Mutual:
+                    return "相互フォロー";
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
