da27488f283b213c80e965f36493648bc34526ab
poketorena <poketorena@users.noreply.github.com>
2018-08-28 17:41:12 +0900

    Refactoring FollowType class

--- FollowManager/Account/FollowType.cs
+++ FollowManager/Account/FollowType.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FollowManager.Account
+﻿namespace FollowManager.Account
 {
+    /// <summary>
+    /// 自分とユーザーの関係
+    /// </summary>
     public enum FollowType
     {
         OneWay,
