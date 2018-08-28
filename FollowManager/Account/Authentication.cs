020bd13af46082f9877ab1a30bce92b41137fc06
poketorena <poketorena@users.noreply.github.com>
2018-08-28 17:40:56 +0900

    Refactoring Authentication class

--- FollowManager/Account/Authentication.cs
+++ FollowManager/Account/Authentication.cs
@@ -1,5 +1,8 @@
 ﻿namespace FollowManager.Account
 {
+    /// <summary>
+    /// CoreTweetのトークンを作るための認証用データ
+    /// </summary>
     public class Authentication
     {
         public string ConsumerKey { get; set; }
