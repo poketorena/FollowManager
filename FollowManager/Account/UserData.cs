8940dcbb4fc9093c0e34bb404843ba9d445a816a
poketorena <poketorena@users.noreply.github.com>
2018-08-28 20:00:51 +0900

    Refactoring UserData class

--- FollowManager/Account/UserData.cs
+++ FollowManager/Account/UserData.cs
@@ -10,15 +10,12 @@ namespace FollowManager.Account
     public class UserData : BindableBase
     {
         // プロパティ
-        private User _user;
         public User User
         {
             get { return _user; }
             set { SetProperty(ref _user, value); }
         }
 
-        [JsonIgnore]
-        private FollowType _followType;
         [JsonIgnore]
         public FollowType FollowType
         {
@@ -27,15 +24,24 @@ namespace FollowManager.Account
         }
 
         [JsonIgnore]
-        private bool _favorite;
-        [JsonIgnore]
         public bool Favorite
         {
             get { return _favorite; }
             set { SetProperty(ref _favorite, value); }
         }
 
+        // プライベート変数
+
+        private User _user;
+
+        [JsonIgnore]
+        private FollowType _followType;
+
+        [JsonIgnore]
+        private bool _favorite;
+
         // コンストラクタ
+
         public UserData(User user, FollowType followType, bool favorite)
         {
             User = user;
