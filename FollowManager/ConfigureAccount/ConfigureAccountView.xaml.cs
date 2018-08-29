457d84f7b49952b75d3cb1b0fd7b2b64e84c7c0e
poketorena <poketorena@users.noreply.github.com>
2018-08-29 16:28:47 +0900

    Change base class of ConfigureAccount to MetroWindow

--- FollowManager/ConfigureAccount/ConfigureAccountView.xaml.cs
+++ FollowManager/ConfigureAccount/ConfigureAccountView.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace FollowManager.ConfigureAccount
 {
     /// <summary>
     /// Interaction logic for ConfigureAccountView.xaml
     /// </summary>
-    public partial class ConfigureAccountView : Window
+    public partial class ConfigureAccountView : MetroWindow
     {
         public ConfigureAccountView()
         {
