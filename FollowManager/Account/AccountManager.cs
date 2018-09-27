4ef06ff2a3398dd71323ace2945f1f861c99353d
poketorena <poketorena@users.noreply.github.com>
2018-09-27 22:19:36 +0900

    Add exception handling to loading and saving authentication data

--- FollowManager/Account/AccountManager.cs
+++ FollowManager/Account/AccountManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using CoreTweet;
 using FollowManager.Collections.Generic;
@@ -51,86 +52,154 @@ namespace FollowManager.Account
             // DI
             _loggingService = loggingService;
 
-            //Current = new Account(_loggingService);
+            // 認証データの読み込み
+            LoadAuthorizationData();
+        }
 
-            //Current.Tokens = Tokens.Create(
-            //    "ConsumerKey",
-            //    "ConsumerSecret",
-            //    "AccessToken",
-            //    "AccessTokenSecret",
-            //    123456789,
-            //    "ScreenName"
-            //    );
+        // デストラクタ
 
-            // 認証データの読み込み（開発用）
+        ~AccountManager()
+        {
+            SaveAuthorizationData();
+        }
 
-            if (File.Exists(@"Data\Authorization\Authorizations.json"))
+        // プライベート関数
+
+        /// <summary>
+        /// 認証データを保存します。
+        /// </summary>
+        private void SaveAuthorizationData()
+        {
+            try
             {
-                using (var streamReader = File.OpenText(@"Data\Authorization\Authorizations.json"))
-                {
-                    var authorizations = new List<Authorization>();
+                var authorizations = new List<Authorization>();
 
+                foreach (var account in Accounts)
+                {
+                    var authorization = new Authorization()
+                    {
+                        ConsumerKey = account.Value.Tokens.ConsumerKey,
+                        ConsumerSecret = account.Value.Tokens.ConsumerSecret,
+                        AccessToken = account.Value.Tokens.AccessToken,
+                        AccessTokenSecret = account.Value.Tokens.AccessTokenSecret,
+                        UserId = account.Value.Tokens.UserId,
+                        ScreenName = account.Value.Tokens.ScreenName
+                    };
+                    authorizations.Add(authorization);
+                }
+
+                const string directoryName = @"Data\Authorization";
+
+                Directory.CreateDirectory(directoryName);
+
+                using (var streamWriter = File.CreateText(@"Data\Authorization\Authorizations.json"))
+                {
                     var settings = new JsonSerializerSettings
                     {
                         Formatting = Formatting.Indented
                     };
-
                     var jsonSerializer = JsonSerializer.Create(settings);
 
-                    authorizations = (List<Authorization>)jsonSerializer.Deserialize(streamReader, typeof(List<Authorization>));
-
-                    foreach (var authorization in authorizations)
-                    {
-                        var account = new Account(_loggingService)
-                        {
-                            Tokens = Tokens.Create(
-                                authorization.ConsumerKey,
-                                authorization.ConsumerSecret,
-                                authorization.AccessToken,
-                                authorization.AccessTokenSecret,
-                                authorization.UserId,
-                                authorization.ScreenName
-                                )
-                        };
-
-                        Accounts.Add((long)account.User.Id,account);
-                    }
+                    jsonSerializer.Serialize(streamWriter, authorizations);
                 }
+            }
+            catch (Exception)
+            {
+                const string errorMessage = "認証データの保存に失敗しました。";
+                _loggingService.Logs.Add(errorMessage);
+                Debug.WriteLine(errorMessage);
             }
         }
 
-        ~AccountManager()
+        /// <summary>
+        /// 認証データを読み込みます。
+        /// </summary>
+        private void LoadAuthorizationData()
         {
-            // 認証データの保存（開発用）
-            var authorizations = new List<Authorization>();
-
-            foreach (var account in Accounts)
+            const string fileName = "Authorizations.json";
+            if (File.Exists($@"Data\Authorization\{fileName}"))
             {
-                var authorization = new Authorization()
+                try
                 {
-                    ConsumerKey = account.Value.Tokens.ConsumerKey,
-                    ConsumerSecret = account.Value.Tokens.ConsumerSecret,
-                    AccessToken = account.Value.Tokens.AccessToken,
-                    AccessTokenSecret = account.Value.Tokens.AccessTokenSecret,
-                    UserId = account.Value.Tokens.UserId,
-                    ScreenName = account.Value.Tokens.ScreenName
-                };
-                authorizations.Add(authorization);
-            }
+                    using (var streamReader = File.OpenText($@"Data\Authorization\{fileName}"))
+                    {
+                        var authorizations = new List<Authorization>();
 
-            var directoryName = @"Data\Authorization";
+                        var settings = new JsonSerializerSettings
+                        {
+                            Formatting = Formatting.Indented
+                        };
 
-            Directory.CreateDirectory(directoryName);
+                        var jsonSerializer = JsonSerializer.Create(settings);
 
-            using (var streamWriter = File.CreateText(@"Data\Authorization\Authorizations.json"))
-            {
-                var settings = new JsonSerializerSettings
+                        authorizations = (List<Authorization>)jsonSerializer.Deserialize(streamReader, typeof(List<Authorization>));
+
+                        foreach (var authorization in authorizations)
+                        {
+                            var account = new Account(_loggingService)
+                            {
+                                Tokens = Tokens.Create(
+                                    authorization.ConsumerKey,
+                                    authorization.ConsumerSecret,
+                                    authorization.AccessToken,
+                                    authorization.AccessTokenSecret,
+                                    authorization.UserId,
+                                    authorization.ScreenName
+                                    )
+                            };
+
+                            Accounts.Add((long)account.User.Id, account);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException)
                 {
-                    Formatting = Formatting.Indented
-                };
-                var jsonSerializer = JsonSerializer.Create(settings);
-
-                jsonSerializer.Serialize(streamWriter, authorizations);
+                    var errorMessage = $"{fileName} を開くことに失敗しました。必要なアクセス許可がありません。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (ArgumentNullException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。指定したパスがnullです。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (ArgumentException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。パスは長さ0の文字列か、空白のみで構成されているか、または1つ以上の正しくない文字を含んでいます。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (PathTooLongException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。指定したパスかファイル名、またはその両方がシステム定義の最大長を超えています。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。指定したパスが無効です。マップされていないドライブを指定していませんか？";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (FileNotFoundException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。指定したパスにファイルが見つかりませんでした。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (NotSupportedException)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。パスの形式が無効です。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
+                catch (Exception)
+                {
+                    var errorMessage = $"{fileName} を開くことに失敗しました。";
+                    _loggingService.Logs.Add(errorMessage);
+                    Debug.WriteLine(errorMessage);
+                }
             }
         }
     }
